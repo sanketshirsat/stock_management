82596e61848e234969c00db40be9b2b6cf3cf0b4
Sanket Shirsat <67065198+sanketshirsat@users.noreply.github.com>
2021-05-27 13:18:48 +0530

    start up project
    Creating maven stock management project

---
+++ src/main/java/com/sanket/stockmger/StockManagementApplication.java
@@ -0,0 +1,13 @@
+package com.sanket.stockmger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockManagementApplication.class, args);
+	}
+
+}
